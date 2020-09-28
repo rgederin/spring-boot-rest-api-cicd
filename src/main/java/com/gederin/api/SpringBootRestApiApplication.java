28e725e0e67ca6880ebfeeec86b58300c9d15ad0
rgederin <gederin.ruslan@gmail.com>
2020-09-28 20:55:17 +0300

    fix build

--- src/main/java/com/gederin/api/SpringBootRestApiApplication.java
+++ src/main/java/com/gederin/api/SpringBootRestApiApplication.java
@@ -8,6 +8,5 @@ public class SpringBootRestApiApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootRestApiApplication.class, args);
-        fake
     }
 }
